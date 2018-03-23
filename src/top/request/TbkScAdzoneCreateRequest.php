db4ea06f463cb8c454f6926817c7e85cdd6ec779
shenyang <juckknife@163.com>
2018-03-23 16:08:30 +0800

    增加 创建广告位接口和超级搜索接口

---
+++ src/top/request/TbkScAdzoneCreateRequest.php
@@ -0,0 +1,68 @@
+<?php
+namespace TopClient\request;
+/**
+ * TOP API: taobao.tbk.sc.adzone.create request
+ * 
+ * @author auto create
+ * @since 1.0, 2018.03.06
+ */
+use TopClient\RequestCheckUtil;
+class TbkScAdzoneCreateRequest
+{
+	/** 
+	 * 广告位名称，最大长度64字符
+	 **/
+	private $adzoneName;
+	
+	/** 
+	 * 网站ID
+	 **/
+	private $siteId;
+	
+	private $apiParas = array();
+	
+	public function setAdzoneName($adzoneName)
+	{
+		$this->adzoneName = $adzoneName;
+		$this->apiParas["adzone_name"] = $adzoneName;
+	}
+
+	public function getAdzoneName()
+	{
+		return $this->adzoneName;
+	}
+
+	public function setSiteId($siteId)
+	{
+		$this->siteId = $siteId;
+		$this->apiParas["site_id"] = $siteId;
+	}
+
+	public function getSiteId()
+	{
+		return $this->siteId;
+	}
+
+	public function getApiMethodName()
+	{
+		return "taobao.tbk.sc.adzone.create";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+		RequestCheckUtil::checkNotNull($this->adzoneName,"adzoneName");
+		RequestCheckUtil::checkMaxLength($this->adzoneName,64,"adzoneName");
+		RequestCheckUtil::checkNotNull($this->siteId,"siteId");
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
