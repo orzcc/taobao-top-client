be21d5c7c23e1d4d335baf1244638943b912338c
orzcc <orzcczh@gmail.com>
2018-09-21 11:47:02 +0800

    订单接口：新增支持订单数据统计类型

--- src/top/request/TbkScOrderGetRequest.php
+++ src/top/request/TbkScOrderGetRequest.php
@@ -4,49 +4,59 @@ namespace TopClient\request;
 use TopClient\RequestCheckUtil;
 /**
  * TOP API: taobao.tbk.sc.order.get request
- * 
+ *
  * @author auto create
  * @since 1.0, 2018.06.13
  */
 class TbkScOrderGetRequest
 {
-	/** 
+	/**
 	 * 需返回的字段列表
 	 **/
 	private $fields;
-	
-	/** 
+
+	/**
+	 * 订单数据统计类型，1: 2方订单，2: 3方订单，如果不设置，或者设置为1，表示2方订单
+	 **/
+	private $orderCountType;
+
+	/**
 	 * 订单查询类型，创建时间“create_time”，或结算时间“settle_time”
 	 **/
 	private $orderQueryType;
-	
-	/** 
+
+	/**
+	 * 订单场景类型，1:常规订单，2:渠道订单，3:会员运营订单，默认为1，通过设置订单场景类型，媒体可以查询指定场景下的订单信息，例如不设置，或者设置为1，表示查询常规订单，常规订单包含淘宝客所有的订单数据，含渠道，及会员运营订单，但不包含3方分成，及维权订单
+	 **/
+	private $orderScene;
+
+	/**
 	 * 第几页，默认1，1~100
 	 **/
 	private $pageNo;
-	
-	/** 
+
+	/**
 	 * 页大小，默认20，1~100
 	 **/
 	private $pageSize;
-	
-	/** 
+
+	/**
 	 * 订单查询时间范围,单位:秒,最小60,最大600,默认60
 	 **/
 	private $span;
-	
-	/** 
+
+	/**
 	 * 订单查询开始时间
 	 **/
 	private $startTime;
-	
-	/** 
+
+	/**
 	 * 订单状态，1: 全部订单，3：订单结算，12：订单付款， 13：订单失效，14：订单成功； 订单查询类型为‘结算时间’时，只能查订单结算状态
 	 **/
 	private $tkStatus;
-	
+
 	private $apiParas = array();
-	
+
 	public function setFields($fields)
 	{
 		$this->fields = $fields;
@@ -58,6 +68,17 @@ class TbkScOrderGetRequest
 		return $this->fields;
 	}
 
+	public function setOrderCountType($orderCountType)
+	{
+		$this->orderCountType = $orderCountType;
+		$this->apiParas["order_count_type"] = $orderCountType;
+	}
+
+	public function getOrderCountType()
+	{
+		return $this->orderCountType;
+	}
+
 	public function setOrderQueryType($orderQueryType)
 	{
 		$this->orderQueryType = $orderQueryType;
@@ -67,6 +88,17 @@ class TbkScOrderGetRequest
 	public function getOrderQueryType()
 	{
 		return $this->orderQueryType;
+	}
+
+	public function setOrderScene($orderScene)
+	{
+		$this->orderScene = $orderScene;
+		$this->apiParas["order_scene"] = $orderScene;
+	}
+
+	public function getOrderScene()
+	{
+		return $this->orderScene;
 	}
 
 	public function setPageNo($pageNo)
@@ -128,20 +160,20 @@ class TbkScOrderGetRequest
 	{
 		return "taobao.tbk.sc.order.get";
 	}
-	
+
 	public function getApiParas()
 	{
 		return $this->apiParas;
 	}
-	
+
 	public function check()
 	{
-		
+
 		RequestCheckUtil::checkNotNull($this->fields,"fields");
 		RequestCheckUtil::checkNotNull($this->span,"span");
 		RequestCheckUtil::checkNotNull($this->startTime,"startTime");
 	}
-	
+
 	public function putOtherTextParam($key, $value) {
 		$this->apiParas[$key] = $value;
 		$this->$key = $value;
