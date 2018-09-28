871ac3b8ebc777dcee40489f41a75ce863eb80b5
orzcc <orzcczh@gmail.com>
2018-09-28 20:45:04 +0800

    新增 渠道ID

--- src/top/request/TbkPrivilegeGetRequest.php
+++ src/top/request/TbkPrivilegeGetRequest.php
@@ -2,40 +2,45 @@
 namespace TopClient\request;
 /**
  * TOP API: taobao.tbk.privilege.get request
- * 
+ *
  * @author auto create
  * @since 1.0, 2017.05.24
  */
 use TopClient\RequestCheckUtil;
 class TbkPrivilegeGetRequest
 {
-	/** 
+	/**
 	 * 推广位id，mm_xx_xx_xx pid三段式中的第三段
 	 **/
 	private $adzoneId;
-	
-	/** 
+
+	/**
 	 * 淘客商品id
 	 **/
 	private $itemId;
-	
-	/** 
+
+	/**
 	 * 营销计划链接中的me参数
 	 **/
 	private $me;
-	
-	/** 
+
+	/**
 	 * 1：PC，2：无线，默认：１
 	 **/
 	private $platform;
-	
-	/** 
+
+	/**
+	 * 渠道关系ID，仅适用于渠道推广场景
+	 **/
+	private $relationId;
+
+	/**
 	 * 备案的网站id, mm_xx_xx_xx pid三段式中的第二段
 	 **/
 	private $siteId;
-	
+
 	private $apiParas = array();
-	
+
 	public function setAdzoneId($adzoneId)
 	{
 		$this->adzoneId = $adzoneId;
@@ -80,6 +85,17 @@ class TbkPrivilegeGetRequest
 		return $this->platform;
 	}
 
+	public function setRelationId($relationId)
+	{
+		$this->relationId = $relationId;
+		$this->apiParas["relation_id"] = $relationId;
+	}
+
+	public function getRelationId()
+	{
+		return $this->relationId;
+	}
+
 	public function setSiteId($siteId)
 	{
 		$this->siteId = $siteId;
@@ -95,20 +111,20 @@ class TbkPrivilegeGetRequest
 	{
 		return "taobao.tbk.privilege.get";
 	}
-	
+
 	public function getApiParas()
 	{
 		return $this->apiParas;
 	}
-	
+
 	public function check()
 	{
-		
+
 		RequestCheckUtil::checkNotNull($this->adzoneId,"adzoneId");
 		RequestCheckUtil::checkNotNull($this->itemId,"itemId");
 		RequestCheckUtil::checkNotNull($this->siteId,"siteId");
 	}
-	
+
 	public function putOtherTextParam($key, $value) {
 		$this->apiParas[$key] = $value;
 		$this->$key = $value;
