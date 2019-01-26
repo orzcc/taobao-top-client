1fb19d939e52fca8b3fd7e315971fbf710b19ad6
orzcc <orzcczh@gmail.com>
2019-01-26 16:46:57 +0800

    新增接口： 处罚订单查询 -导购-私域用户管理专用

---
+++ src/top/request/TbkDgPunishOrderGetRequest.php
@@ -0,0 +1,50 @@
+<?php
+namespace TopClient\request;
+use TopClient\RequestCheckUtil;
+
+/**
+ * TOP API: taobao.tbk.dg.punish.order.get request
+ *
+ * @author auto create
+ * @since 1.0, 2019.01.21
+ */
+class TbkDgPunishOrderGetRequest
+{
+	/**
+	 * 入参的对象
+	 **/
+	private $afOrderOption;
+
+	private $apiParas = array();
+
+	public function setAfOrderOption($afOrderOption)
+	{
+		$this->afOrderOption = $afOrderOption;
+		$this->apiParas["af_order_option"] = $afOrderOption;
+	}
+
+	public function getAfOrderOption()
+	{
+		return $this->afOrderOption;
+	}
+
+	public function getApiMethodName()
+	{
+		return "taobao.tbk.dg.punish.order.get";
+	}
+
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+
+	public function check()
+	{
+
+	}
+
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
