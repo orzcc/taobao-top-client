f2127e8ca0380d366e4c247cee1d65415aaead03
orzcc <orzcczh@gmail.com>
2019-02-12 10:05:55 +0800

    新增淘宝客维权退款订单查询（渠道订单专享）

---
+++ src/top/request/TbkRelationRefundRequest.php
@@ -0,0 +1,48 @@
+<?php
+namespace TopClient\request;
+/**
+ * TOP API: taobao.tbk.relation.refund request
+ *
+ * @author auto create
+ * @since 1.0, 2019.01.18
+ */
+class TbkRelationRefundRequest
+{
+	/**
+	 * 参数option
+	 **/
+	private $searchOption;
+
+	private $apiParas = array();
+
+	public function setSearchOption($searchOption)
+	{
+		$this->searchOption = $searchOption;
+		$this->apiParas["search_option"] = $searchOption;
+	}
+
+	public function getSearchOption()
+	{
+		return $this->searchOption;
+	}
+
+	public function getApiMethodName()
+	{
+		return "taobao.tbk.relation.refund";
+	}
+
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+
+	public function check()
+	{
+
+	}
+
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
