fa3252566c22f384995efdbcdefccd1fe06cb800
orzcc <orzcczh@gmail.com>
2019-02-17 18:17:52 +0800

    TbkDgOptimusMaterialRequest 接口更新

--- src/top/request/TbkDgOptimusMaterialRequest.php
+++ src/top/request/TbkDgOptimusMaterialRequest.php
@@ -2,60 +2,65 @@
 namespace TopClient\request;
 /**
  * TOP API: taobao.tbk.dg.optimus.material request
- * 
+ *
  * @author auto create
- * @since 1.0, 2018.09.13
+ * @since 1.0, 2019.01.24
  */
 use TopClient\RequestCheckUtil;
 class TbkDgOptimusMaterialRequest
 {
-	/** 
+	/**
 	 * mm_xxx_xxx_xxx的第三位
 	 **/
 	private $adzoneId;
-	
-	/** 
+
+	/**
 	 * 内容详情ID
 	 **/
 	private $contentId;
-	
-	/** 
+
+	/**
 	 * 内容渠道信息
 	 **/
 	private $contentSource;
-	
-	/** 
+
+	/**
 	 * 设备号加密类型：MD5
 	 **/
 	private $deviceEncrypt;
-	
-	/** 
+
+	/**
 	 * 设备号类型：IMEI，或者IDFA，或者UTDID
 	 **/
 	private $deviceType;
-	
-	/** 
+
+	/**
 	 * 设备号加密后的值
 	 **/
 	private $deviceValue;
-	
-	/** 
+
+	/**
+	 * 商品ID，用于相似商品推荐
+	 **/
+	private $itemId;
+
+	/**
 	 * 官方的物料Id(详细物料id见：https://tbk.bbs.taobao.com/detail.html?appId=45301&postId=8576096)
 	 **/
 	private $materialId;
-	
-	/** 
+
+	/**
 	 * 第几页，默认：1
 	 **/
 	private $pageNo;
-	
-	/** 
+
+	/**
 	 * 页大小，默认20，1~100
 	 **/
 	private $pageSize;
-	
+
 	private $apiParas = array();
-	
+
 	public function setAdzoneId($adzoneId)
 	{
 		$this->adzoneId = $adzoneId;
@@ -122,6 +127,17 @@ class TbkDgOptimusMaterialRequest
 		return $this->deviceValue;
 	}
 
+	public function setItemId($itemId)
+	{
+		$this->itemId = $itemId;
+		$this->apiParas["item_id"] = $itemId;
+	}
+
+	public function getItemId()
+	{
+		return $this->itemId;
+	}
+
 	public function setMaterialId($materialId)
 	{
 		$this->materialId = $materialId;
@@ -159,20 +175,20 @@ class TbkDgOptimusMaterialRequest
 	{
 		return "taobao.tbk.dg.optimus.material";
 	}
-	
+
 	public function getApiParas()
 	{
 		return $this->apiParas;
 	}
-	
+
 	public function check()
 	{
-		
+
 		RequestCheckUtil::checkNotNull($this->adzoneId,"adzoneId");
 		RequestCheckUtil::checkMaxValue($this->pageSize,100,"pageSize");
 		RequestCheckUtil::checkMinValue($this->pageSize,1,"pageSize");
 	}
-	
+
 	public function putOtherTextParam($key, $value) {
 		$this->apiParas[$key] = $value;
 		$this->$key = $value;
